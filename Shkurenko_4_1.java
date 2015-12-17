abd6253b6d5baf4effeb50b470b7395fac8ab764
Andrey Shkurenko <mebius1987@gmail.com>
2015-12-17 15:02:51 +0200

    reweno s 1 po 12 primer

--- Shkurenko_4_1.java
+++ Shkurenko_4_1.java
@@ -58,7 +58,8 @@ public class Shkurenko_4_1 {
 		// 4) a / b
 
 		// 5) a % b
-		
+		// При выборе соответствующего пункта меню происходит вывод результата
+		// вычисления (должна быть проверка деления на 0!)
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Введите первое вещественное число");
 		double a = sc.nextDouble();
@@ -69,10 +70,25 @@ public class Shkurenko_4_1 {
 		System.out.println("3)" + a + " * " + b);
 		System.out.println("4)" + a + " / " + b);
 		System.out.println("5)" + a + " % " + b);
+		System.out.println("");
+		System.out.println("Введите какое действие требуется выполнить");
+		int c = sc.nextInt();
+		if (c > 0 && c < 6) {
+			if (c == 1) {
+				System.out.println("1)" + a + " + " + b + "=" + (a + b));
+			}else if (c == 2) {
+				System.out.println("2)" + a + " - " + b + "=" + (a - b));
+			}else if (c == 3) {
+				System.out.println("3)" + a + " * " + b + "=" + (a * b));
+			}else if (c == 4 && a != 0 && b != 0) {
+				System.out.println("4)" + a + " / " + b + "=" + (a / b));
+			}else if (c == 5) {
+				System.out.println("5)" + a + " % " + b + "=" + (a * (b/100)));
+			}
+		}
 		
-		// При выборе соответствующего пункта меню происходит вывод результата
-		// вычисления (должна быть проверка деления на 0!)
 		*/
+		
 		/*
 		// 5.​ Ввести с клавиатуры символ. Определить, чем он является: цифрой,
 		// буквой или знаком пунктуации.
@@ -116,6 +132,7 @@ public class Shkurenko_4_1 {
 		} else 
 		System.out.println("Число " + X + " не кратно числу " + Y);
 		*/
+		/*
 		// 8.​ Ввести число и определить кратно ли оно 3, 5, и 7 одновременно.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Введите число для определения кратности 3, 5, 7");
@@ -124,21 +141,69 @@ public class Shkurenko_4_1 {
 			System.out.println("Число " + chislo + " кратно 3, 5, 7 одновременно");
 		} else 
 			System.out.println("Число " + chislo + " не кратно 3, 5, 7 одновременно");
-
+		*/
+		/*
 		// 9.​ Показать модуль введённого числа.
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите число");
+		int a = sc.nextInt();
+		if (a > 0){
+			System.out.println("Модуль числа " + a + " = " + a);
+		} else if (a < 0){
+			System.out.println("Модуль числа " + a + " = " + (-a));
+			
+		}
+		*/
+		/*
 		// 10.​ Вводится целое число (не более 4 разрядов!). Определить
 		// количество цифр в нём.
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите число не больше 4 разрядов");
+		int a = sc.nextInt();
+		if ((a %10) == a){
+			System.out.println("Число содержит 1 цифру");
+		} else if ((a %100) == a){
+			System.out.println("Число содержит 2 цифры");
+		} else if ((a %1000) == a){
+			System.out.println("Число содержит 3 цифры");
+		} else if ((a %10000) == a){
+			System.out.println("Число содержит 4 цифры");
+		} else
+		System.out.println("Не соблюдены условия \"Вводится целое число (не более 4 разрядов!)\" ");
+		*/
+		/*
 		// 11.​ Ввести с клавиатуры пятизначное число и определить, является ли
 		// оно палиндромом (т.е. одинаково читается в обоих направлениях -
 		// например, 12321 будет палиндромом, а 12345 – не палиндром).
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите пятизначное число для определения является ли оно палиндромом");
+		int a = sc.nextInt();
+		if ((a / 10000) == (a % 10) && (a / 1000 % 10) == ( a / 10 % 10)){
+			System.out.println("Число " + a + " является палиндромом");
+		} else
+			System.out.println("Число " + a + " не является палиндромом");
+		*/
 		// 12.​ Пользователь задаёт координаты верхнего левого, и нижнего
 		// правого угла прямоугольника, а также координаты точки (X,Y) в
 		// декартовой системе координат. Принадлежит ли точка этому
 		// прямоугольнику?
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите координату X для верхнего левого  угла прямоугольника");
+		double xkor1 = sc.nextDouble();
+		System.out.println("Введите координату Y для верхнего левого  угла прямоугольника");
+		double ykor1 = sc.nextDouble();
+		System.out.println("Введите координаты x для нижнего правого угла прямоугольника");
+		double xkor2 = sc.nextDouble();
+		System.out.println("Введите координаты y для нижнего правого угла прямоугольника");
+		double ykor2 = sc.nextDouble();
+		System.out.println("Введите координаты точки x");
+		double x = sc.nextDouble();
+		System.out.println("Введите координаты точки y");
+		double y = sc.nextDouble();
+		if (x <= xkor1 && x >= xkor2 && y <= ykor1 && y >= ykor2 ){
+			System.out.println("Точка принадлежит прямоугольнику");
+		} else
+			System.out.println("Точка не принадлежит прямоугольнику");
 		// 13.​ Найти максимальное значение среди 4 переменных, используя
 		// тернарный оператор.
 
